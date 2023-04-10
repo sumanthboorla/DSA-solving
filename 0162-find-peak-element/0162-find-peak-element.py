63ae2c242723875843b8d7131ca011a067c07f73
SUMANTH BOORLA <73389805+sumanthboorla@users.noreply.github.com>
2023-04-10 23:30:05 +0530

    Time: 42 ms (89.65%), Space: 14.1 MB (31.27%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -10,15 +10,6 @@ class Solution:
                 start=mid+1
         return start
         
-        
-#         start, end = 0, len(nums) - 1
-#         while(start<end):
-#             mid=start+(end-start)//2
-# 			if(nums[mid]>nums[mid+1]):
-#                 end=mid
-# 			else:
-# 				start=mid+1
-#         return start
-                
+
             
         
